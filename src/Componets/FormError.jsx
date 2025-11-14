f751844c55f5c186404774c394d7ec83c84a14fe
tomassotables <info@tomassotables.com>
2025-11-14 16:08:55 +0530

    First Comment

--- src/Componets/FormError.jsx
+++
@@ -1,5 +0,0 @@
-// src/components/FormError.jsx
-export default function FormError({ message }) {
-  if (!message) return null;
-  return <p style={{ color: 'red', marginTop: '5px' }}>{message}</p>;
-}
